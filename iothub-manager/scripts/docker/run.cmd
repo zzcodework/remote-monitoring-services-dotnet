fb4d9516b4e95e4e2f6a716b87a3b9ffe996b59c
Xiangzhi Sheng <xisheng@microsoft.com>
2017-08-02 09:56:47 +0800

    Rename docker image to azureiotpcs/iothub-manager-dotnet (#61)

--- iothub-manager/scripts/docker/run.cmd
+++ iothub-manager/scripts/docker/run.cmd
@@ -1,7 +1,7 @@
 @ECHO off & setlocal enableextensions enabledelayedexpansion
 
 :: Note: use lowercase names for the Docker images
-SET DOCKER_IMAGE="azureiotpcs/iothubmanager-dotnet"
+SET DOCKER_IMAGE="azureiotpcs/iothub-manager-dotnet"
 
 :: strlen("\scripts\docker\") => 16
 SET APP_HOME=%~dp0
