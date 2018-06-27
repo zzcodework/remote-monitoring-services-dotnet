9c88d0e6dffdb37791418bff796dc80312eea6ac
Aayush Gupta <t-aagupt@microsoft.com>
2018-06-27 16:55:27 -0700

    Updated RulesTest.cs

--- device-telemetry/Services.Test/RulesTest.cs
+++ device-telemetry/Services.Test/RulesTest.cs
@@ -87,15 +87,12 @@ namespace Services.Test
 
             var sampleActions = new List<IActionItem>
             {
-                //new ActionItem()
-                //{
-                //    ActionType = Type.Email,
-                //    Parameters = new Dictionary<string, object>()
-                //    {
-                //        {"email", new List<string>(){"sampleEmail@gmail.com", "sampleEmail2@gmail.com"}},
-                //        {"subject", "Test Email"}
-                //    }
-                //}
+                
+                new EmailActionItem(Type.Email, new Dictionary<string, object>()
+                    {
+                        {"email", new List<string>(){"sampleEmail@gmail.com", "sampleEmail2@gmail.com"}},
+                        {"subject", "Test Email"}
+                    })
             };
 
             var sampleRules = new List<Rule>
