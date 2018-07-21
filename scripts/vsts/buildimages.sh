58a1c9843ab3a6aed3f201f0268c67089d01cc88
Sushil Raje <suraje@microsoft.com>
2018-07-20 19:48:01 -0700

    CI/CD scripts

---
+++ scripts/vsts/buildimages.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+# Copyright (c) Microsoft. All rights reserved.
+# Note: Windows Bash doesn't support shebang extra params
+set -e
+
+APP_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../../ && pwd )/"
+
+servicestobuild=$(IdentifyMS.services)
+declare -A microservicefolders
+
+microservicefolders+=(
+        ["asamanager"]="asa-manager"
+        ["pcsauth"]="pcs-auth"
+        ["pcsconfig"]="pcs-config"
+        ["iothubmanager"]="iothub-manager"
+        ["pcsstorageadapter"]="pcs-storage-adapter"
+        ["devicetelemetry"]="device-telemetry"
+        ["devicesimulation"]="device-simulation"
+)
+
+build()
+{
+    IFS=','; microservices=($servicestobuild); unset IFS;
+    for microservice in ${!microservices[@]}; do
+        msfolder=${microservices[${microservice}]}
+	    location=${microservicefolders[${msfolder}]}
+		cd $location
+		scripts/docker/build
+		cd ..
+    done
+}
+
+build
