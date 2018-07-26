359177b99ba31925abf5b0d9d19f0d05705b285e
Jill Bender <jbend@microsoft.com>
2018-07-26 10:04:36 -0700

    Add Role-based Policies and ability to request AllowedActions for a User (#36)
    * Add user policy config to service
    
    * Read policies from file and return allowed actions with user
    
    * Update roles in Users to read all values into an array
    
    * whitespace corrections in Users.cs
    
    * Add PolicyList service model for parsing JSON and update Admin policies to include new allowed Actions
    
    * revert DotSettings changes
    
    * Add docs for policies
    
    * remove comment about sym link
    
    * add comment about custom policies in config
    
    * Update docs for policies with information about aad manifest and fields
    
    * Update order to CUD for AllowedActions
    
    * add documentation on token claims
    
    * remove en-us from url
    
    * Refactor loop to LINQ expression
    
    * fix typos in docs
    
    * Add test for User class
    
    * Update mapRelativePath to use Path.Combine and add more detailed error messages
    
    * update order of Role and Id in POLICIES.md
    
    * use hash set to prevent duplicate allowed actions
    
    * fix whitespace

--- pcs-auth/Services/Models/Policy.cs
+++ pcs-auth/Services/Models/Policy.cs
@@ -4,11 +4,17 @@ using System.Collections.Generic;
 
 namespace Microsoft.Azure.IoTSolutions.Auth.Services.Models
 {
-    public class User
+    public class Policy
     {
         public string Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
+        public string Role { get; set; }
         public List<string> AllowedActions { get; set; }
+
+        public Policy()
+        {
+            this.Id = string.Empty;
+            this.Role = string.Empty;
+            this.AllowedActions = new List<string>();
+        }
     }
 }
