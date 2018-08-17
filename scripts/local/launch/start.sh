3ff5f3ed59097ce073d3b698f34a76e0bee4baeb
sushilraje <suraje@microsoft.com>
2018-08-16 22:49:51 -0700

    vscode tabs  update

--- scripts/local/launch/start.sh
+++ scripts/local/launch/start.sh
@@ -31,20 +31,20 @@ if [ $azres -ne 0 ]; then
    read  -n 1 -p "Have you created required Azure resources (Y/N)?" yn
    echo -e "\n"
 
-   case $yn in
-	   "Y") 
-			echo -e "Please set the env variables in .env file.\n The file is located under scripts/local folder.";  
-			exit 0
-		;;
-	   "N") 
-			echo "Setting up Azure resources."; 
-			$APP_HOME/scripts/local/launch/helpers/create_azure_resources.sh;
-		;;
-		*)
-			echo "Incorrect option. Please re-run the script."
-			exit 0
-		;;
-   esac
+	case $yn in
+	"Y") 
+		echo -e "Please set the env variables in .env file.\n The file is located under scripts/local folder.";  
+		exit 0
+	;;
+	"N") 
+		echo "Setting up Azure resources."; 
+		$APP_HOME/scripts/local/launch/helpers/create_azure_resources.sh;
+	;;
+	*)
+		echo "Incorrect option. Please re-run the script."
+		exit 0
+	;;
+	esac
 fi
 
 set +e
