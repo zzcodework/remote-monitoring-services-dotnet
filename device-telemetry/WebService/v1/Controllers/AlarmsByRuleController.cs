3e7630f4e2f1a053429635bf83b145e91b6ddf6b
Molly Moen <momoe@microsoft.com>
2018-09-24 15:44:33 -0700

    remove unused using

--- device-telemetry/WebService/v1/Controllers/AlarmsByRuleController.cs
+++ device-telemetry/WebService/v1/Controllers/AlarmsByRuleController.cs
@@ -4,7 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.Devices;
 using Microsoft.Azure.IoTSolutions.DeviceTelemetry.Services;
 using Microsoft.Azure.IoTSolutions.DeviceTelemetry.Services.Diagnostics;
 using Microsoft.Azure.IoTSolutions.DeviceTelemetry.Services.Models;
