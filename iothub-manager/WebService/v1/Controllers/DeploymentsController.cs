3055de5523a02fa1ad38c17d3f41fffd0aa90934
Molly Moen <momoe@microsoft.com>
2018-11-14 15:24:36 -0800

    Add missing readall tag
    Add ReadAll tag missing from deploymentscontroller

--- iothub-manager/WebService/v1/Controllers/DeploymentsController.cs
+++ iothub-manager/WebService/v1/Controllers/DeploymentsController.cs
@@ -67,6 +67,7 @@ namespace Microsoft.Azure.IoTSolutions.IotHubManager.WebService.v1.Controllers
         /// <param name="includeDeviceStatus">Whether to retrieve additional details regarding device status</param>
         /// <returns>Deployment information with metrics</returns>
         [HttpGet("{id}")]
+        [Authorize("ReadAll")]
         public async Task<DeploymentApiModel> GetAsync(string id, [FromQuery] bool includeDeviceStatus = false)
         {
             return new DeploymentApiModel(await this.deployments.GetAsync(id, includeDeviceStatus));
