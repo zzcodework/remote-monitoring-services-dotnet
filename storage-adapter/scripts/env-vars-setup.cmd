e0ec47930ad0d0b328eeaec612694e4ec99b4b40
sushilraje <suraje@microsoft.com>
2019-03-25 16:00:26 -0700

    updates

--- storage-adapter/scripts/env-vars-setup.cmd
+++
@@ -1,8 +0,0 @@
-:: Copyright (c) Microsoft. All rights reserved.
-
-@ECHO off & setlocal enableextensions enabledelayedexpansion
-
-:: Prepare the environment variables used by the application.
-
-:: DocumentDb connection string
-SETX PCS_STORAGEADAPTER_DOCUMENTDB_CONNSTRING "..."
